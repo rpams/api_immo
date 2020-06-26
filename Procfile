9f592d9376fc2af907f703f494188ec74b6ed860
rpams <romapam@gmail.com>
2020-06-26 17:52:58 +0200

    new config

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn api-immo.wsgi --log-file -
--- api_immo/settings.py
+++ api_immo/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 from datetime import timedelta
+import django_heroku
+
+django_heroku.settings(locals())
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -166,6 +169,8 @@ REST_FRAMEWORK = {
 
 # For the rewind views
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
 
